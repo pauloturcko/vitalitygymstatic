6fd3c29b10a910f30e7039f21eae8b8fc8c0b4d6
Paulo Turco <pauloturcko@gmail.com>
2024-11-13 21:28:24 -0300

    Project Theme Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'main-color': '#E40046',
+      'bg-color01': '#242424',
+      'bg-color02': '#353535',
+      'text-color': '#F2F2F2'
+    },
+    fontFamily: {
+        'main-font': ['lowvetica', 'sans-serif'],
+        'secondary-font': ['Barlow', 'sans-serif'],
+        'text-font': ['Lato', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
